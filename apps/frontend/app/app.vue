f82b01698cb985cfa1418d0887b1f06e137c2a7b
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-10-24 14:10:09 +0200

    feat: integrate i18n support with localization for English and French, add Tailwind CSS and color mode modules, and update main application structure

--- apps/frontend/app/app.vue
+++ apps/frontend/app/app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>{{ $t('welcome') }}</h1>
+    <div class="flex gap-2">
+      <button class="border border-slate-500 p-2 rounded-xl" v-for="locale in locales" @click="setLocale(locale.code)">
+        {{ locale.name }}
+      </button>
+    </div>
   </div>
 </template>
+<script setup lang="ts">
+const { locales, setLocale } = useI18n()
+</script>
--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    'pinia-plugin-persistedstate',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode'
+  ],
+  i18n: {
+    locales: [
+      { code: 'en', language: 'en-US', file: 'en.json', name: 'English' },
+      { code: 'fr', language: 'fr-FR', file: 'fr.json', name: 'Français' },
+    ],
+    defaultLocale: 'en',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root' // recommended
+    }
+  }
 })
